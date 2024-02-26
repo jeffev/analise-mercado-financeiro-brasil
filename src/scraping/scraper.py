6c1b140b6e1df913d4a82a24cbc543fa77e1e5a3
Jefferson Valandro <jeffev123@gmail.com>
2024-02-26 15:14:35 -0300

    data estraida do site pelo scrapper

--- src/scraping/scraper.py
+++ src/scraping/scraper.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import os
+import time
 
 def coletar_indicadores(ticker):
     driver = webdriver.Chrome()
@@ -11,20 +13,51 @@ def coletar_indicadores(ticker):
     url = f"https://statusinvest.com.br/acoes/{ticker}"
     driver.get(url)
 
+    # Espera para a página carregar
+    time.sleep(2)
+
     try:
+        # Tenta fechar pop-ups de propaganda, se existirem
+        popups = driver.find_elements(By.CSS_SELECTOR, ".popup-fixed .btn-close")
+        for popup in popups:
+            try:
+                # Tenta clicar no botão de fechar do popup
+                WebDriverWait(driver, 3).until(EC.element_to_be_clickable(popup)).click()
+                print("Popup fechado com sucesso.")
+                # Espera um pouco para garantir que o popup seja fechado
+                time.sleep(1)  # Reduz o tempo de espera se não for necessário tanto
+            except Exception as e:
+                # Captura qualquer exceção que possa ocorrer durante o fechamento do popup
+                print("Erro ao fechar popup:", str(e))
+
         # Aguarda até que o botão Histórico esteja presente e clica nele
         historico_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//button[@title="Histórico do ativo"]'))
         )
         historico_button.click()
         
+        # Após clicar no histórico, aguarda 2 segundos
+        time.sleep(2)
+
+        # Utiliza JavaScript para clicar no botão "Máx."
+        max_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'li[title="Máximo disponível"] a'))
+        )
+        driver.execute_script("arguments[0].click();", max_button)
+
+        # Após clicar no MAX, aguarda 2 segundos
+        time.sleep(2)
+
         WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'table-history'))
         )
+
+        # Cria o diretório se ele não existir
+        os.makedirs('data/raw', exist_ok=True)
         
         # Prepara o arquivo CSV para escrita
-        nome_arquivo = f'indicadores_{ticker}.csv'
-        with open(nome_arquivo, 'w', newline='', encoding='utf-8') as file:
+        filepath = os.path.join('data', 'raw', f'indicadores_{ticker}.csv')
+        with open(filepath, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             
             # Identifica todos os grupos de indicadores
@@ -67,7 +100,6 @@ tickers = [
     "B3SA3",  # B3 S.A. - Brasil, Bolsa, Balcão
     "WEGE3",  # WEG S.A.
     "MGLU3",  # Magazine Luiza S.A.
-    "VVAR3",  # Via Varejo S.A.
     "BBAS3",  # Banco do Brasil S.A.
     "GGBR4",  # Gerdau S.A.
     "CSNA3",  # Companhia Siderúrgica Nacional
